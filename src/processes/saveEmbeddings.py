975c45e980c99f969f0c81342dcda6d82ee8830f
VARUN46 <vsetia46@gmail.com>
2024-06-02 16:11:04 +0530

    Pdf read and chunk methods

--- src/processes/saveEmbeddings.py
+++ src/processes/saveEmbeddings.py
@@ -1,7 +1,8 @@
 import http
 import http.client
 import json
-
+import glob
+import PyPDF2
 
 def generateEmbeddingsOllama(text):
     conn = http.client.HTTPConnection("localhost", 11434)
@@ -20,12 +21,28 @@ def generateEmbeddingsOllama(text):
     return vector['embedding']
 
 
+def readAllFilesInDir(dir):
+    files = glob.glob(dir+"*.pdf")
+    return files
+    
+def convertPdfToText(pdfPath):
+    text = ''
+    pagesLen = len(reader.pages)
+    reader = PyPDF2.PdfReader(pdfPath)
+    for pageNum in range(pagesLen):
+        text = text + reader.pages[pageNum].extract_text()
+    return text
+    
+def splitFileText(text, size):
+    currentPos = 0
+    chunk = []
+    lastPos = len(text)
+    while currentPos < lastPos:
+        chunk.append(text[currentPos : currentPos + size])
+        currentPos = currentPos + size
+    return chunk
 
-def readAllFiles():
-    pass
 
 def saveEmbeddings(embeddings):
     pass    
 
-embeddings = generateEmbeddingsOllama('hello')
-print(embeddings)

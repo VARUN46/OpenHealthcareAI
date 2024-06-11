64b40ec1b492bdccd3586c61f8d104f7b80efd88
VARUN46 <vsetia46@gmail.com>
2024-06-11 21:50:32 +0530

    db proc updates

--- src/database/procedures/usp_getRelevantChunks.sql
+++ src/database/procedures/usp_getRelevantChunks.sql
@@ -7,5 +7,21 @@ CONTAINS SQL
 SQL SECURITY DEFINER
 COMMENT ''
 BEGIN
-	SELECT JSON_DEPTH(embeddingVector);
+	SELECT * FROM JSON_TABLE(
+         embeddingVector,
+         "$[*]"
+         COLUMNS(
+           Pos FOR ORDINALITY,
+           Value FLOAT PATH "$"
+         )
+       ) data;
+
+	CREATE TEMPORARY TABLE relevant_docs(
+		similarity_score FLOAT,
+		document_text TEXT CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL
+	);
+	
+	
+	SELECT * FROM relevant_docs;
+	
 END
